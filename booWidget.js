a59122639a4e36c630c8b84a8c01cd024d9be285
SolentLibrary <65720201+SolentLibrary@users.noreply.github.com>
2020-06-02 12:01:28 +0100

    Update booWidget.js

--- booWidget.js
+++ booWidget.js
@@ -2,6 +2,7 @@
 	xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     document.getElementById("booWidget").innerHTML = this.responseText;
+	 console.log(this.responseText);
   }
 };
 xhttp.open("GET", "https://solentLibrary.github.io/boo-query-gen/booContent.html", true);
